90a2168870b89f58e43d9f0dda4c4b0f115dd1a2
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-09 15:28:39 -0600

    compiles

--- src/Models.hs
+++ src/Models.hs
@@ -1,23 +1,29 @@
-module Models () where
+module Models
+  (
+  ) where
 
-import qualified Data.UUID as Uuid
-import Data.UUID.V4 (nextRandom)
-import qualified Data.Text as T
+import qualified Data.Text       as T
 import qualified Data.Time.Clock as Time
+import qualified Data.UUID       as Uuid
+import           Data.UUID.V4    (nextRandom)
 
-newtype Content = Content T.Text deriving (Show, Eq) -- need ToJson if you add servant
+newtype Content =
+  Content T.Text
+  deriving (Show, Eq) -- need ToJson if you add servant
+
 type TodoID = Uuid.UUID
 
-data Todo = 
-  Pending { _content :: Content
-          , _createdAt :: Time.UTCTime
-          , _id :: TodoID
-          } |
-  Completed  { _content :: Content
-             , _createdAt :: Time.UTCTime
-             , _finishedAt :: Time.UTCTime
-             , _id :: TodoID
-             }
+data Todo
+  = Pending { _content   :: Content
+            , _createdAt :: Time.UTCTime
+            , _id        :: TodoID }
+  | Completed { _content    :: Content
+              , _createdAt  :: Time.UTCTime
+              , _finishedAt :: Time.UTCTime
+              , _id         :: TodoID }
 
 newTodo :: T.Text -> IO Todo
-newTodo inputText = Pending (Content inputText) <$> Time.getCurrentTime <$> nextRandom
+newTodo inputText = do
+  time <- Time.getCurrentTime
+  id <- nextRandom
+  return $ Pending (Content inputText) time id
