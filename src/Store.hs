eff65e074a3bb676ea202d62e47feef06a82a569
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-10 20:43:39 -0500

    closer?

--- src/Store.hs
+++ src/Store.hs
@@ -6,7 +6,13 @@ import Models
 
 type Store = Map TodoID Todo
 
+isPending :: Todo -> Bool
+isPending (Pending a b c) = True -- don't need a b c. How to just check which kind in ADT?
+isPending x = False
+
 init :: Store
 init = Map.empty
 
 listPending :: Store -> [Todo]
+listPending store = pendings where
+  pendings = Map.elems $ filter isPending store -- this is yelling at me
