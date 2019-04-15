ac4e08b18ed143e99ee84405a0040bd9e1af4e7b
Drew Boardman <drew.e.boardman@gmail.com>
2019-04-15 18:08:28 -0500

    newtypes

--- src/Models.hs
+++ src/Models.hs
@@ -18,9 +18,9 @@ import qualified Data.UUID       as Uuid
 import           Data.UUID.V4    (nextRandom)
 import           GHC.Generics    (Generic)
 
-type TodoID = Uuid.UUID
-
+newtype TodoID = TodoID Uuid.UUID deriving (Eq, Show, Generic)
 newtype Content = Content T.Text deriving (Show, Eq, Generic)
+newtype CompletedTime = CompletedTime Time.UTCTime deriving (Show, Eq, Generic)
 
 data Todo = PendingTodo Pending | CompletedTodo Completed deriving (Generic)
 
@@ -30,20 +30,22 @@ data Pending = Pending { _content   :: Content
 
 data Completed = Completed { _content    :: Content
                            , _createdAt  :: Time.UTCTime
-                           , _finishedAt :: Time.UTCTime
+                           , _finishedAt :: CompletedTime
                            , _id         :: TodoID } deriving (Eq, Show, Generic)
 
 instance ToJSON Todo
+instance ToJSON TodoID
 instance ToJSON Completed
+instance ToJSON CompletedTime
 instance ToJSON Pending
 instance ToJSON Content
 
 newTodo :: T.Text -> IO Pending
 newTodo inputText = do
   time <- Time.getCurrentTime
-  Pending (Content inputText) time <$> nextRandom
+  Pending (Content inputText) time . TodoID <$> nextRandom
 
 completePendingTodo :: Pending -> IO Completed
 completePendingTodo (Pending content created todoID) = do
   time <- Time.getCurrentTime
-  return $ Completed content created time todoID
+  return $ Completed content created (CompletedTime time) todoID
