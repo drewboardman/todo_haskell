c7063df53a7d80a16217b1a9fe572b7d0102a0fd
Drew Boardman <drew.e.boardman@gmail.com>
2019-04-15 19:17:43 -0500

    got the server runninggit add .

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Seeder (seed)
+import TodoController (app)
+import Network.Wai.Handler.Warp
 
 main :: IO ()
-main = seed
+main = run 8081 app
--- src/controllers/TodoController.hs
+++ src/controllers/TodoController.hs
@@ -1,11 +1,21 @@
 {-# LANGUAGE DataKinds     #-}
 {-# LANGUAGE TypeOperators #-}
 
-module TodoController () where
+module TodoController (app) where
 
-import qualified Models      as M (Pending, Completed)
-import           Servant ((:>), Get, JSON)
+import qualified Models  as M (Completed, Pending)
+import           Servant ((:>), Application, Get, JSON, Proxy (Proxy), Server,
+                          serve)
 
 type TodoAPI =
   -- /todos/all -- returns all Todos
   "todos" :> "all" :> Get '[JSON] ([M.Pending], [M.Completed])
+
+app :: Application
+app = serve todoAPI todoServer
+
+todoAPI :: Proxy TodoAPI
+todoAPI = Proxy
+
+todoServer :: Server TodoAPI
+todoServer = return ([], [])
