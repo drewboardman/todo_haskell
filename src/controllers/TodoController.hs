f37163fa908d7cab7af89f30ff828e1f7cfd8c91
Drew Boardman <drew.e.boardman@gmail.com>
2019-04-22 14:28:26 -0500

    i did this alone without any help from anyone whatsoever

--- src/controllers/TodoController.hs
+++ src/controllers/TodoController.hs
@@ -1,22 +1,17 @@
 {-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators       #-}
 
 module TodoController (app) where
 
-import           Control.Monad.Except
+import           Control.Monad.Except   (join, throwError)
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Maybe             (fromMaybe)
 import qualified Data.UUID              as Uuid
 import qualified Models                 as M (AllTodos, Todo, TodoID (TodoID))
-import           Servant
--- import           Servant                    ((:<|>), (:>), Application, Get,
---                                              JSON, Proxy (Proxy), QueryParam,
---                                              ServantErr, Server, serve)
+import           Servant                ((:<|>) ((:<|>)), (:>), Application,
+                                         Get, Handler, JSON, Proxy (Proxy),
+                                         QueryParam, Server, err404, serve)
 import qualified TodoActions            as Actions (allTodos, singleTodo)
-
-type ServantMonad a b = ExceptT ServantErr a b
 
 type TodoAPI =
   -- /todos/all -- returns all Todos
@@ -33,10 +28,16 @@ todoAPI = Proxy
 todoServer :: Server TodoAPI
 todoServer = todos :<|> todo where
 
-    todos :: IO M.AllTodos
+    todos :: Handler M.AllTodos
     todos = liftIO Actions.allTodos
 
-    todo :: Uuid.UUID -> ServantMonad IO M.Todo
+    todo :: Maybe Uuid.UUID -> Handler M.Todo
     todo uuid = do
-      maybeTodo :: Maybe M.Todo <- Actions.singleTodo $ M.TodoID uuid
-      fromMaybe (throwError "cannot find todo with the uuid") maybeTodo
+      maybeTodo <- liftIO $ traverse (Actions.singleTodo . M.TodoID) uuid
+      thing $ join maybeTodo
+
+-- thing :: (Monad m, MonadError ServantErr m, IsString e) => Maybe M.Todo -> m M.Todo
+thing ::  Maybe M.Todo -> Handler M.Todo
+thing x = case x of
+  Just todo -> return todo
+  Nothing   -> throwError err404
